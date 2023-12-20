8edbf476a47f605626c8365f371fb2a850d8c22b
Kixsume <47682228+kixsume@users.noreply.github.com>
2023-12-19 20:38:07 -0600

    Update config_flow.py

--- custom_components/custom_llm_conversation/config_flow.py
+++ custom_components/custom_llm_conversation/config_flow.py
@@ -96,7 +96,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             _LOGGER.exception("Unexpected exception")
             errors["base"] = "unknown"
         else:
-            return self.async_create_entry(title="Custom OpenAI Conversation", data=user_input)
+            return self.async_create_entry(title="Custom LLM Conversation", data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
